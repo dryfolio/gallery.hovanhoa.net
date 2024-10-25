7ac354211a68fde9e1d9123d4372fdd2542d42ed
hovanhoa <hovanhoaa1@gmail.com>
2024-10-25 21:09:30 +0700

    feat: customize

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   images: {
-    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
-        protocol: "https",
         hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/my-account/**",
       },
     ],
   },
