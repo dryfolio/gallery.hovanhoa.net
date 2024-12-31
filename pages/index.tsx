e87fd05bba01caf3f4c4a47ec016ca6c4ccaced6
hovanhoa <hovanhoaa1@gmail.com>
2024-12-31 14:13:20 +0700

    feat: update nav color

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,9 @@ import { useLastViewedPhoto } from "../utils/useLastViewedPhoto";
 import {BASE_URL, IMAGE, NAME} from "../constants";
 import Navbar from "../components/nav";
 import Hero from "../components/hero";
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   const router = useRouter();
@@ -33,60 +36,62 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
       <Head>
         <title>hovanhoa | gallery</title>
       </Head>
-      <main className="min-h-screen relative pt-8">
-        <section className="relative mx-auto max-w-3xl px-4 sm:px-6 lg:px-8 overflow-hidden mb-10">
-          <div className="items-center flex justify-between mb-6">
-            <Link href={BASE_URL}>
-              <Image
-                  src={IMAGE}
-                  alt={NAME}
-                  className="h-12 w-12 rounded-full"
-                  height={100}
-                  width={100}
-              />
-            </Link>
-            <Navbar/>
-          </div>
-        </section>
-        {photoId && (
-            <Modal
-                images={images}
-                onClose={() => {
-                  setLastViewedPhoto(photoId);
-                }}
-            />
-        )}
-        <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          {images.map(({id, public_id, format, blurDataUrl}) => (
-              <Link
-                  key={id}
-                  href={`/?photoId=${id}`}
-                  as={`/p/${id}`}
-                  ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
-                  shallow
-                  className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-              >
+      <div className={inter.className}>
+        <main className="min-h-screen relative pt-8">
+          <section className="relative mx-auto max-w-3xl px-4 sm:px-6 lg:px-8 overflow-hidden mb-10">
+            <div className="items-center flex justify-between mb-6">
+              <Link href={BASE_URL}>
                 <Image
-                    alt="Next.js Conf photo"
-                    className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-                    style={{transform: "translate3d(0, 0, 0)"}}
-                    placeholder="blur"
-                    blurDataURL={blurDataUrl}
-                    src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
-                    width={720}
-                    height={480}
-                    sizes="(max-width: 640px) 100vw,
+                    src={IMAGE}
+                    alt={NAME}
+                    className="h-12 w-12 rounded-full"
+                    height={100}
+                    width={100}
+                />
+              </Link>
+              <Navbar/>
+            </div>
+          </section>
+          {photoId && (
+              <Modal
+                  images={images}
+                  onClose={() => {
+                    setLastViewedPhoto(photoId);
+                  }}
+              />
+          )}
+          <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+            {images.map(({id, public_id, format, blurDataUrl}) => (
+                <Link
+                    key={id}
+                    href={`/?photoId=${id}`}
+                    as={`/p/${id}`}
+                    ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
+                    shallow
+                    className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+                >
+                  <Image
+                      alt="Next.js Conf photo"
+                      className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+                      style={{transform: "translate3d(0, 0, 0)"}}
+                      placeholder="blur"
+                      blurDataURL={blurDataUrl}
+                      src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+                      width={720}
+                      height={480}
+                      sizes="(max-width: 640px) 100vw,
                 (max-width: 1280px) 50vw,
                 (max-width: 1536px) 33vw,
                 25vw"
-                />
-              </Link>
-          ))}
-        </div>
-      </main>
-      <footer className="p-6 text-center text-white/80 sm:p-12">
+                  />
+                </Link>
+            ))}
+          </div>
+        </main>
+        <footer className="p-6 text-center text-white/80 sm:p-12">
 
-      </footer>
+        </footer>
+      </ div>
     </>
   );
 };
@@ -95,10 +100,10 @@ export default Home;
 
 export async function getStaticProps() {
   const results = await cloudinary.v2.search
-    .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-    .sort_by("public_id", "desc")
-    .max_results(400)
-    .execute();
+      .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
+      .sort_by("public_id", "desc")
+      .max_results(400)
+      .execute();
   let reducedResults: ImageProps[] = [];
 
   let i = 0;
