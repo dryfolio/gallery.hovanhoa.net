a41fabfe1b83dd6323bae367e97c6a9ab2ac64c2
hovanhoa <hovanhoaa1@gmail.com>
2024-12-31 15:20:22 +0700

    feat: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+import { BASE_URL } from '../constants'
+
+export default function _404() {
+    return (
+        <div className="grid h-screen place-content-center bg-background px-4 text-center gap-5">
+            <div>
+                <h1 className="text-7xl font-black text-slate-900">404</h1>
+            </div>
+            <Link href={BASE_URL} className="text-sky-600">
+                <button>Go Back Home</button>
+            </Link>
+        </div>
+    )
+}
