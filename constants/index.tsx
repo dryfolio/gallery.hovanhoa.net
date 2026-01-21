ab50667a7e8587b9400f7e82a5ad764fdaea37be
hovanhoa <hovanhoaa1@gmail.com>
2026-01-21 15:08:48 +0700

    remove status

--- constants/index.tsx
+++ constants/index.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 
 const NAME = 'hovanhoa'
 const BASE_URL = 'https://hovanhoa.net'
-const STATUS_URL = 'https://status.hovanhoa.net'
 const INSIGHT_URL = 'https://insight.hovanhoa.net'
 const GALLERY_URL = 'https://gallery.hovanhoa.net'
 const TWITTER = '_hovanhoa_'
@@ -80,6 +79,5 @@ export {
     OG_IMAGE_BG,
     BASE_URL,
     INSIGHT_URL,
-    STATUS_URL,
     ROLE,
 }
