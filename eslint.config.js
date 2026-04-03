69414ac09de4b965940902c0d0211e7f22c5b5be
hovanhoa <hovanhoaa1@gmail.com>
2026-04-03 10:18:29 +0700

    chore: remove unused @eslint/js dependency from package.json and package-lock.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
 const { FlatCompat } = require("@eslint/eslintrc");
-const js = require("@eslint/js");
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
