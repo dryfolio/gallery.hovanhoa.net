a25c444298786de3491e52764ede8dddc14dd59c
hovanhoa <hovanhoaa1@gmail.com>
2026-04-03 10:24:54 +0700

    Cache image

--- utils/cachedImages.ts
+++ utils/cachedImages.ts
@@ -1,16 +1,21 @@
+import fs from "fs";
+import path from "path";
 import cloudinary from "./cloudinary";
 
 let cachedResults;
 
 export default async function getResults() {
   if (!cachedResults) {
-    const fetchedResults = await cloudinary.v2.search
-      .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-      .sort_by("filename", "desc")
-      .max_results(400)
-      .execute();
-
-    cachedResults = fetchedResults;
+    const cachePath = path.join(process.cwd(), "utils", "images.cache.json");
+    if (fs.existsSync(cachePath)) {
+      cachedResults = JSON.parse(fs.readFileSync(cachePath, "utf-8"));
+    } else {
+      cachedResults = await cloudinary.v2.search
+        .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
+        .sort_by("filename", "desc")
+        .max_results(400)
+        .execute();
+    }
   }
 
   return cachedResults;
