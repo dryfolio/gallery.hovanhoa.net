9e40a5cf6022560e095658d1596afcb8b907b190
hovanhoa <hovanhoaa1@gmail.com>
2026-04-03 09:56:11 +0700

    chore: migrate ESLint configuration from .eslintrc.json to eslint.config.js and update dependencies

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+const { FlatCompat } = require("@eslint/eslintrc");
+const js = require("@eslint/js");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+];
+
+module.exports = eslintConfig;
