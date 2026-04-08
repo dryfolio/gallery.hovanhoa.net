a01c301e6ea2aaf15a7cd4137a821f4469b7e085
hovanhoa <hovanhoaa1@gmail.com>
2026-04-08 13:42:14 +0700

    Update sitemap for gallery

--- pages/sitemap.ts
+++ pages/sitemap.ts
@@ -1,46 +1,66 @@
 import { GetServerSideProps } from 'next'
 import { GALLERY_URL, BASE_URL, INSIGHT_URL } from '../constants'
+import getResults from '../utils/cachedImages'
 
 const INFO_URL = 'https://info.hovanhoa.net'
 const STATUS_URL = 'https://status.hovanhoa.net'
+const MUSIC_URL = 'https://music.hovanhoa.net'
 
-function generateSitemap() {
+function generateSitemap(imageResults: any) {
+  const resources = imageResults?.resources || []
+  const now = new Date().toISOString()
+
   const staticPages = [
     {
       url: GALLERY_URL,
-      lastModified: new Date().toISOString(),
+      lastModified: now,
       changeFrequency: 'weekly',
       priority: 1.0,
     },
     {
       url: BASE_URL,
-      lastModified: new Date().toISOString(),
+      lastModified: now,
       changeFrequency: 'always',
       priority: 0.9,
     },
     {
       url: INSIGHT_URL,
-      lastModified: new Date().toISOString(),
+      lastModified: now,
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
       url: INFO_URL,
-      lastModified: new Date().toISOString(),
+      lastModified: now,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: STATUS_URL,
-      lastModified: new Date().toISOString(),
+      lastModified: now,
       changeFrequency: 'always',
+      priority: 0.8,
+    },
+    {
+      url: MUSIC_URL,
+      lastModified: now,
+      changeFrequency: 'daily',
       priority: 0.8,
     },
   ]
 
+  const photoPages = resources.map((img: any, index: number) => ({
+    url: `${GALLERY_URL}/p/${index}`,
+    lastModified: img.uploaded_at || now,
+    changeFrequency: 'monthly',
+    priority: 0.6,
+  }))
+
+  const allPages = [...staticPages, ...photoPages]
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${staticPages
+${allPages
   .map(
     (page) => `  <url>
     <loc>${page.url}</loc>
@@ -56,7 +76,8 @@ ${staticPages
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  const sitemap = generateSitemap()
+  const imageResults = await getResults()
+  const sitemap = generateSitemap(imageResults)
 
   res.setHeader('Content-Type', 'text/xml')
   res.write(sitemap)
