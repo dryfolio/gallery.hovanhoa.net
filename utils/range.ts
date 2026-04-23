486ddd44e5e857d46ed73e2e94baadb46600c806
hovanhoa <hovanhoaa1@gmail.com>
2026-04-23 11:47:23 +0700

    little update

--- utils/range.ts
+++ utils/range.ts
@@ -1,6 +1,6 @@
 export const range = (start: number, end: number): number[] => {
   const output: number[] = [];
-  for (let i = start; i < end; i += 1) {
+  for (let i = start; i <= end; i += 1) {
     output.push(i);
   }
   return output;
